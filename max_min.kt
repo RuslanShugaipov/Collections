62029fcfd74950410e4032fae5c48d452a0a4273
RuslanShugaipov <nabon1812@mail.ru>
2022-03-09 08:48:36 +0000

    Add new file

---
+++ max_min.kt
@@ -0,0 +1,6 @@
+// Return a customer who has placed the maximum amount of orders
+fun Shop.getCustomerWithMaxOrders(): Customer? =
+        customers.maxByOrNull { it.orders.count() }
+
+// Return the most expensive product that has been ordered by the given customer
+fun getMostExpensiveProductBy(customer: Customer): Product? =  customer.orders.flatMap(Order::products).maxByOrNull(Product::price)
